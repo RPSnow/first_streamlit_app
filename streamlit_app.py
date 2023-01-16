81df8a5be5f63c9a44cb2783f3e2250023668dd0
RPSnow <122718311+RPSnow@users.noreply.github.com>
2023-01-16 11:54:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,9 +44,10 @@ def get_fruit_load_list():
    with  my_cnx.cursor() as my_cur:
       my_cur.execute("select * from fruit_load_list")
       return  my_cur.fetchall()
-if streamlit.button('Get fruit load list'):
+if streamlit.button('View Our Fruit List - Add Your Favorites!'):
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    my_data_rows =get_fruit_load_list()
+   my_cnx.close()
    streamlit.dataframe(my_data_rows)
 
 # streamlit.stop()
