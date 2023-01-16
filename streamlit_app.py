e15bd77f66852b3440ca8a1cd96826ee4f23e857
RPSnow <122718311+RPSnow@users.noreply.github.com>
2023-01-16 10:51:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,3 +38,7 @@ my_data_rows = my_cur.fetchall()
 # streamlit.text("Hello from Snowflake:")
 streamlit.header("The Fruit load list contains: ")
 streamlit.dataframe(my_data_rows)
+
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('Thank you for adding ', add_my_fruit)
