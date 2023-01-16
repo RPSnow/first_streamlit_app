d6413933bb868ac003e4b8786be774a742a89e99
RPSnow <122718311+RPSnow@users.noreply.github.com>
2023-01-16 11:45:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,11 +49,18 @@ if streamlit.button('Get fruit load list'):
    my_data_rows =get_fruit_load_list()
    streamlit.dataframe(my_data_rows)
 
-streamlit.stop()
+# streamlit.stop()
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 # streamlit.text("Hello from Snowflake:")
 
+def insert_row_sowflake(new_fruit):
+   with  my_cnx.cursor() as my_cur:
+      my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values('from stramlit')")
+      return "Thanks for addig "+new_fruit
+       
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add fruit to the list'):
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   back_from_function =insert_row_sowflake(add_my_fruit)
+   streamlit.text('Thank you for adding ', back_from_function)
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thank you for adding ', add_my_fruit)
-my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values('from stramlit')")
